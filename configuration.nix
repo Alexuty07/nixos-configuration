1d7a85467952fb896072af01f54fa91498167a01
Álex Santiago <52899490+Alexuty07@users.noreply.github.com>
2024-03-13 23:13:02 -0400

    Edit SDDM config

--- configuration.nix
+++ configuration.nix
@@ -52,7 +52,9 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.desktopManager.plasma6.enable = true;
+
   # Enable and configure SDDM
+  services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.displayManager.sddm.autoNumlock = true;
 
