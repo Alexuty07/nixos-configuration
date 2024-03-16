c6d7887dc415456d8789405ebc1eb5f524127fa9
Álex Santiago <52899490+Alexuty07@users.noreply.github.com>
2024-03-16 05:38:28 +0000

    add the first flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # To enable Home Manager later when i feel like doing it
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
