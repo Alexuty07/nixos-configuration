c3ca1f023544fdec6a2d2ca447c89f08817925fa
Álex Santiago <alexuty@proton.me>
2024-04-13 11:38:08 -0400

    Add power management options

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,25 @@
   # LUKS devices
   boot.initrd.luks.devices."luks-1ec6d49d-7a0b-4ac9-aaea-e8efc1c75ac0".device = "/dev/disk/by-uuid/1ec6d49d-7a0b-4ac9-aaea-e8efc1c75ac0";
 
+  # Laptop power management
+  services.thermald.enable = true
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 20;
+        };
+  };
+
+
   # Autoupgrade
   system.autoUpgrade = {
     enable = true;
